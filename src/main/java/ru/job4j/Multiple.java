22fadd0d92ced64d994c88537b264daf2dbd9ccc
KatyaBahai <katyxa-two@yandex.ru>
2022-10-17 21:47:31 -0500

    deleted the extra line

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -13,6 +13,5 @@ public class Multiple {
         System.out.println("1 * 8 = 8");
         System.out.println("1 * 9 = 9");
         System.out.println("1 * 10 = 10");
-        double distance = 3.2143e7;
     }
 }

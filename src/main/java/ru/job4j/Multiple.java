82cbb5bc228600504bf3c8b73449b454d72ac919
KatyaBahai <katyxa-two@yandex.ru>
2022-10-17 21:23:31 -0500

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -13,5 +13,6 @@ public class Multiple {
         System.out.println("1 * 8 = 8");
         System.out.println("1 * 9 = 9");
         System.out.println("1 * 10 = 10");
+        double distance = 3.2143e7;
     }
 }

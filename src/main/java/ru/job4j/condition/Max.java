67471a020a9262cff9282631c310625e6ece4da0
KatyaBahai <katyxa-two@yandex.ru>
2022-11-08 16:47:27 -0600

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -5,4 +5,27 @@ public class Max {
     public static int max(int left, int right) {
         return left > right ? left : right;
     }
+
+    public static int max(int first, int second, int third) {
+        int result = third;
+        if (first >= second && first >= third) {
+            result = first;
+        }
+        if (second >= first && second >= third) {
+            result = second;
+        }
+        return result;
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        int result;
+        if (first >= second && first >= third && first >= fourth) {
+            result = first;
+        } else if (second >= first && second >= third && second >= fourth) {
+            result = second;
+        } else {
+            result = third >= fourth ? third : fourth;
+        }
+        return result;
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -40,4 +40,67 @@ class MaxTest {
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenMax2To3To4Then4() {
+        int one = 2;
+        int two = 3;
+        int three = 4;
+        int result = Max.max(one, two, three);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax3To3To3Then3() {
+        int one = 2;
+        int two = 3;
+        int three = 3;
+        int result = Max.max(one, two, three);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax3To3To4Then4() {
+        int one = 3;
+        int two = 3;
+        int three = 4;
+        int result = Max.max(one, two, three);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax9To2To4To8Then9() {
+        int one = 9;
+        int two = 2;
+        int three = 4;
+        int four = 8;
+        int result = Max.max(one, two, three, four);
+        int expected = 9;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax2To2To2To8Then8() {
+        int one = 2;
+        int two = 2;
+        int three = 2;
+        int four = 8;
+        int result = Max.max(one, two, three, four);
+        int expected = 8;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax9To9To9To9Then9() {
+        int one = 9;
+        int two = 9;
+        int three = 9;
+        int four = 9;
+        int result = Max.max(one, two, three, four);
+        int expected = 9;
+        assertThat(result).isEqualTo(expected);
+    }
 }
